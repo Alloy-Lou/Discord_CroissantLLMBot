26f858a070a4d1576eb14a651ab4de54fcd97618
Alloy <alloy.ytb@gmail.com>
2026-05-31 21:01:18 +0200

    v1.0

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
-const commandFiles = fs.readdirSync(commandFolders).filter((file) => file.endsWith('.js'));
+const commandFiles = commandFolders.filter((file) => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const filePath = path.join(commandFolders, file);
+    const filePath = path.join(foldersPath, file);
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
